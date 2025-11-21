a980fa2f93b0820669c15fd38de7818bf07edcfe
prashant <chaubeyprashant20@gmail.com>
2025-11-22 03:43:22 +0530

    ui updated with animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { motion, useInView, AnimatePresence } from 'framer-motion';
 
 const Home = () => {
